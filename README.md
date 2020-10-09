c05320b0972592be251b48004cc48592b77d3ca4
Dario Alves Junior <darioajr@gmail.com>
2020-10-08 23:09:05 -0300

    add docs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # firebase-domain-txt
 Firebase Cloud Function to check existing TXT in domain
+
+# build
+npm run build
+
+# emulator tests
+firebase emulators:start
+http://localhost:5001/firebase-domain-txt/us-central1/getAllTXT?addresses=incident.com.br
+
+# deploy
+firebase deploy --only functions
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,12 @@
 import * as functions from 'firebase-functions';
-import * as dns from "dns";
+import * as dns from 'dns';
 
-export const getAllTXT = functions.https.onRequest((request, response) => {
-    let domain: any;
-    domain = request.query.addresses;
-    dns.resolveTxt(domain, function (err, addresses) {
+exports.getAllTXT = functions.https.onRequest((request, response) => {
+    const domain: any = request.query.addresses;
+    dns.resolveTxt(domain, (err, addresses) => {
             response.send(`TXT records: ${addresses}`);
             response.json({ result: `TXT records: ${addresses}` });
             functions.logger.info('TXT records: %j', addresses, { structuredData: true });
-        }
+    });
 });
 
